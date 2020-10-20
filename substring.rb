72567d2f458dff2e4c093b49d543e8fa7124285c
Joon Kim <jkim527@gmail.com>
2020-10-20 22:47:28 +0900

    Add pseudocode

---
+++ substring.rb
@@ -0,0 +1,25 @@
+def substrings(text, dictionary)
+    text #turn to lowercase
+    text_array = text.chars
+    substring_array = []
+    substring_hash = {}
+
+
+    
+    
+    dictionary.each do |word|
+        if text.match(/word/)
+            push #push to substring_array
+
+
+
+    # for each word in dictionary, if there is a sequence in the text that matches, push the word to substring_array
+    # for each word in substring_array, push the word to substring_hash, add to value (counter)
+
+
+    # for each word in dictionary 1) set counter, 2) look for sequence of characters within array that match word, 3) add to counter
+    # create hash and add key value pair to hash? ONLY if word in dictionary has > 0 for counter
+    # or from the beginning add word in dictionary to hash as key (if exists) and then add the value as each 
+
+
+    
