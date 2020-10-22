2dbb04cc8e91c0fcf702b61471aa521aa9a06557
Joon Kim <jkim527@gmail.com>
2020-10-22 19:29:01 +0900

    Fix bug of not counting repeat words

--- substring.rb
+++ substring.rb
@@ -2,24 +2,26 @@ text = "Howdy partner, sit down! How's it going?"
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(text, dictionary)
-    text.downcase! # convert text to lowercase for #match 
+    text.downcase! # convert text to lowercase to look for matches
     substring_array = [] 
-    substring_hash = Hash.new(0)    
+    substring_hash = Hash.new(0) # initialize hash with default value of 0 instead of nil
     
-    for i in 0..(dictionary.length - 1) do # search for dictionary[i] among characters in text_array
-        if text.include?(dictionary[i])
-            substring_array.push(dictionary[i])
+    for i in 0..(dictionary.length - 1) do # search for each word in dictionary among characters in text
+        while text.include?(dictionary[i])
+            substring_array.push(dictionary[i]) # add words that are a match. Need to fix: make sure the program doesn't break after each match
+            text.slice!(dictionary[i])
         end
+
+        text = "Howdy partner, sit down! How's it going?"
     end
 
-    substring_array.each do |v|
-        substring_hash[v] += 1
+    substring_array.each do |v| # push words in substring_array to substring_hash, increasing the value for each repeated key
+        substring_hash[v] += 1 
     end
 
-    puts substring_hash
+    puts substring_hash # print hash
 end
 
 substrings(text, dictionary)
 
-    # for each word in dictionary, if there is a sequence in the text that matches, push the word to substring_array
-    # for each word in substring_array, push the word to substring_hash, add to value (counter)
+# if text.include?(dictionary[i]) 
